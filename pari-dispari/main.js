d3c793887e850b2e4d6df267230b028c77a40ea7
Marco <marco.faltoni@outlook.it>
2020-04-24 22:19:34 +0200

    Esercizio Pari Dispari fatto

--- pari-dispari/main.js
+++ pari-dispari/main.js
@@ -1,12 +1,5 @@
 // gioco pari o dispari contro il pc: chiedere all'utente un numero tra 1 e 5 e generare un numero random tra 1 e 5. Chiedere all'utente se sceglie pari o dispari, quindi fare la somma dei due numeri (giocata utente + giocata pc), quindi valutare se il risultato è pari o dispari e comunicare all'utente se ha vinto o meno
 
-var giocata_utente = parseInt(prompt('Scegli un numero intero tra 1 e 5'));
-console.log("la giocata dell'utente è " + giocata_utente);
-
-var giocata_pc = generaRandom(1, 5);
-console.log('la giocata del pc è ' + giocata_pc);
-
-
 // funzione per generare un numero random in un intervallo deciso da me o dall'utente
 function generaRandom(min, max) {
     var numero = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -18,8 +11,43 @@ function sommaNumeri(num1,num2) {
     var somma = num1 + num2;
 }
 
+// funzione per verificare se un numero è pari o dispari
+// restituisce true se il numero passato in input è pari, false altrimenti
+function isEven(numero) {
+    var resto = numero % 2;
+    if(resto == 0) {
+        // il numero è pari
+        // console.log('pari');
+        return true;
+    } else {
+        // il numero è dispari
+        // console.log('dispari');
+        return false;
+    }
+}
+
+var giocata_utente = parseInt(prompt('Scegli un numero intero tra 1 e 5'));
+console.log("la giocata dell'utente è " + giocata_utente);
+
+var giocata_pc = generaRandom(1, 5);
+console.log('la giocata del pc è ' + giocata_pc);
+
+var pari_disapri = prompt('Ora scegli: pari o dispari?');
+
 sommaNumeri(giocata_utente, giocata_pc);
 var risultato_somma = giocata_utente + giocata_pc;
 console.log('la somma di ' + giocata_utente + ' e ' + giocata_pc + ' vale: ' + risultato_somma);
+var risultato_somma = isEven(risultato_somma);
 
-var giocata_utente2 = prompt('Scegli tra pari e dispari')
+
+if (risultato_somma == true && pari_disapri == 'pari'){
+    console.log('Hai vinto');
+} if (risultato_somma == true && pari_disapri == 'dispari') {
+    console.log('Hai perso');
+}
+
+if (risultato_somma == false && pari_disapri == 'dispari') {
+    console.log('Hai vinto');
+} if  (risultato_somma == false && pari_disapri == 'pari') {
+    console.log('Hai perso');
+}
